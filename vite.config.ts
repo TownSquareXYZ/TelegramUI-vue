4f349b13c2836ec6920502d391582b0d9b5f36bf
ccddzzw <ccddzzw@gmail.com>
2024-11-08 12:47:15 +0800

    change compile tool from  swc  to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'TelegramUIVue',
+      formats: mode === 'cjs' ? ['cjs'] : ['es'],
+      fileName: (format) => `${format === 'cjs' ? 'cjs/' : ''}index.js`
+    },
+    rollupOptions: {
+      external: ['vue', '@floating-ui/vue', '@twa-dev/types'],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: ({ name }) => {
+          return `${name}.js`
+        },
+        chunkFileNames: ({ name }) => {
+          return `${name}.js`
+        },
+        assetFileNames: ({ name }) => {
+          return `${name}.[ext]`
+        },
+        globals: {
+          vue: 'Vue',
+          '@floating-ui/vue': 'FloatingUIVue'
+        }
+      }
+    },
+    sourcemap: true,
+    outDir: mode === 'cjs' ? 'dist/cjs' : 'dist'
+  }
+}))
